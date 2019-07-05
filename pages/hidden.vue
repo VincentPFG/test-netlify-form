acec7cef593d2a6511da3ce7fbf835c8ff9a13b6
VincentPFG <vincentgiraud@live.fr>
2019-07-05 20:58:49 +0200

    correction faute ortho netlify

--- pages/hidden.vue
+++ pages/hidden.vue
@@ -1,6 +1,6 @@
 <template lang='pug'>
 div
-     form(name='hidden' netflify hidden)#form
+     form(name='hidden' netlify hidden)#form
           input(name='form-name' value='hidden')
           input(name='message' v-model='form.message')
      v-form
