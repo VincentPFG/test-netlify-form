2a7676064572d9d1cc2c3b6a1bdd79b345f17b50
VincentPFG <vincentgiraud@live.fr>
2019-07-07 13:09:37 +0200

    dialog

--- pages/hidden.vue
+++ pages/hidden.vue
@@ -7,21 +7,41 @@ div
      //-      v-text-field(v-model='form.message' label='message')
      //-      v-btn(type='submit' form='form') submit
 
-     v-form(name='hidden' netlify)
-          div(hidden)
-               input(name='form-name' value='hidden')
-               input(name='message' v-model='form.message')
-               input(name='name' v-model='form.name')
-               input(name='email' v-model='form.email')
-          v-container
-               v-layout(wrap)
-                    v-flex(xs12 lg6)
-                         v-text-field(v-model='form.name' label='name')
-                    v-flex(xs12 lg6)
-                         v-text-field(v-model='form.email' label='email')
-               v-textarea(v-model='form.message' label='message')
+     //- v-form(name='hidden' netlify)
+     //-      div(hidden)
+     //-           input(name='form-name' value='hidden')
+     //-           input(name='message' v-model='form.message')
+     //-           input(name='name' v-model='form.name')
+     //-           input(name='email' v-model='form.email')
+     //-      v-container
+     //-           v-layout(wrap)
+     //-                v-flex(xs12 lg6)
+     //-                     v-text-field(v-model='form.name' label='name')
+     //-                v-flex(xs12 lg6)
+     //-                     v-text-field(v-model='form.email' label='email')
+     //-           v-textarea(v-model='form.message' label='message')
+     //-           v-layout(justify-center)
+     //-                v-btn(type='submit').center submit
+
+     v-dialog(v-model='dialog')
+          template(v-slot:activator='{on}')
                v-layout(justify-center)
-                    v-btn(type='submit').center submit
+                    v-btn(v-on='on') laisser un message
+          v-form(name='hidden' netlify)
+               div(hidden)
+                    input(name='form-name' value='hidden')
+                    input(name='message' v-model='form.message')
+                    input(name='name' v-model='form.name')
+                    input(name='email' v-model='form.email')
+               v-container
+                    v-layout(wrap)
+                         v-flex(xs12 lg6)
+                              v-text-field(v-model='form.name' label='name')
+                         v-flex(xs12 lg6)
+                              v-text-field(v-model='form.email' label='email')
+                    v-textarea(v-model='form.message' label='message')
+                    v-layout(justify-center)
+                         v-btn(type='submit') submit
 </template>
 
 <script lang='coffee'>
@@ -31,6 +51,7 @@ export default
                message: ''
                name: ''
                email: ''
+          dialog: off
      methods:
           encode: (data) -> ("#{encodeURIComponent key}=#{encodeURIComponent data[key]}" for key of data).join '&'
           submit: ->
