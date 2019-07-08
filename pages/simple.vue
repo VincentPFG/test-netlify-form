7a7ff2267a65fea2c184d3e70251f87e782d5d27
VincentPFG <vincentgiraud@live.fr>
2019-07-08 14:39:55 +0200

    civil

--- pages/simple.vue
+++ pages/simple.vue
@@ -2,9 +2,11 @@
 v-form(netlify name='simple')
      div(hidden)
           input(name='form-name' value='simple')
+          input(name='civil' v-model='civil')
           input(name='name' v-model='name')
           input(name='email' v-model='email')
           textarea(name='message' v-model='message')
+     v-select(:items='choix' label='civil' v-model='civil')
      v-text-field(label='name' v-model='name')
      v-text-field(label='email' v-model='email')
      v-textarea(label='message' v-model='message')
@@ -15,7 +17,10 @@ v-form(netlify name='simple')
 <script lang='coffee'>
 export default
      data: ->
+          civil: ''
           name: ''
           email: ''
           message: ''
+
+          choix: ['madame', 'monsieur']
 </script>
