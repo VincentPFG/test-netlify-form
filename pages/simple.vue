e73b04fbb5d1fc2e95af978c7331715ea5095a2d
VincentPFG <vincentgiraud@live.fr>
2019-07-08 13:46:36 +0200

    simple

---
+++ pages/simple.vue
@@ -0,0 +1,20 @@
+<template lang='pug'>
+form(netlify name='simple')
+     div(hidden)
+          input(type='text' name='name' v-model='name')
+          input(type='email' name='email' v-model='email')
+          textarea(name='message' v-model='message')
+     v-text-field(label='name' v-model='name')
+     v-text-field(label='email' v-model='email')
+     v-textarea(label='message' v-model='message')
+
+     v-btn(type='submit') submit
+</template>
+
+<script lang='coffee'>
+export default
+     data: ->
+          name: ''
+          email: ''
+          message: ''
+</script>
